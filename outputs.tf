01ceae89dca08197640e2664f021f6f9c7f82335
Erik Andreazi <andreazi.erik@gmail.com>
2024-01-21 20:30:25 -0300

    Adicionando outputs referente ao dominio criado

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "public-ip" {
   value = module.ec2.public-ip
 }
+
+output "dns" {
+  value = module.route53.dns-record
+}
+
+output "db_name" {
+  value = {
+    db      = module.rds.database,
+    address = module.rds.endpoint,
+    user    = module.rds.db-username,
+  }
+}
